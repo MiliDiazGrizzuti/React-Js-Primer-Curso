962d31b10afdf0145789399bae2cb491d4f8e51d
MiliDiazGrizzuti <milagros.diazgrizzuti@hotmail.com>
2023-03-30 08:38:52 -0300

    proyectofinal

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>PINKYS</h3>
+            <div>
+                <button>Indumentaria</button>
+                <button>Accesorios</button>
+                <button>Para el mate</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
